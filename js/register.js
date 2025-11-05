5e1bbeaddaf8e7b53219396df6ad5ecec17e5ab9
Elizabeth <lian28.tekistekis@gmail.com>
2025-11-05 21:19:24 +0800

    adding register / signup

---
+++ js/register.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDxTSnDc-z4wJ4fL9zf3kB3uuvZjcISNjQ",
+  authDomain: "login-agriknows.firebaseapp.com",
+  projectId: "login-agriknows",
+  storageBucket: "login-agriknows.firebasestorage.app",
+  messagingSenderId: "281355587751",
+  appId: "1:281355587751:web:fb479b62b5036b44b68b82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+//inputs
+const email = document.getElementsById ('email').value;
+const password = document.getElementsById ('password').value;
+
+//submit button /  signup button
+const submit = document.getElementsById ('submit');  
+submit.addEventListener("click",function (event){
+ event.preventDefeault()
+ alert(5)
+})
