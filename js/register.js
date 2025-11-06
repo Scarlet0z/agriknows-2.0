166b0d27e17c24c6467fc1edfe83d1b401de2e00
Elizabeth <lian28.tekistekis@gmail.com>
2025-11-06 18:59:26 +0800

    working button on sign up

--- js/register.js
+++ js/register.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,18 +13,33 @@ const firebaseConfig = {
   projectId: "login-agriknows",
   storageBucket: "login-agriknows.firebasestorage.app",
   messagingSenderId: "281355587751",
-  appId: "1:281355587751:web:fb479b62b5036b44b68b82"
+  appId: "1:281355587751:web:fb479b62b5036b44b68b82",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-//inputs
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
+const auth = getAuth(app);
 
 //submit button /  signup button
-const submit = document.getElementById('submit');  
-submit.addEventListener("click", function (event){
- event.preventDefault();
+const submit = document.getElementById("submit");
+submit.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  //inputs
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      alert("Creating Account...");
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+      // ..
+    });
 });
