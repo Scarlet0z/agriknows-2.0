507c33eb2649f88b188be6986ffb2e2133c5d588
Elizabeth <lian28.tekistekis@gmail.com>
2025-11-06 20:27:17 +0800

    register js changed

--- js/register.js
+++ js/register.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
-import {getAuth,createUserWithEmailAndPassword,} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
